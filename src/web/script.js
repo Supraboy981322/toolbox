e92290a1922dee15681bd52478e0aeed4653f54a
keeper <97927259+Supraboy981322@users.noreply.github.com>
2025-12-29 18:46:50 -0600

    start rewriting web ui
    was unsatisfied with the old ui, and I got a better idea. some things're broke, but I need to commit before I forget

--- src/web/script.js
+++ src/web/script.js
@@ -1,21 +1,32 @@
-ptItems = document.querySelectorAll(".itm").length;
-for (i = 0; i < ptItems.length; i++) {
-  elm = ptItems[i];
-  pt = elm.querySelector(".pt"); 
-  pt.addEventListener('click', (event) => {
-    event.stopPropagation();
-  });
-}
+const endPtPane = document.getElementById("endPtPane");
 
-function dropDown(elm, event) {
-  elmClass = event.target.className; 
-  if (elmClass != "ptName" && elmClass != "itm") {
-    return;
-  }
-  ptDiv = elm.querySelector(".pt");
-  if (elm.getAttribute("open") == "true") {
-    elm.setAttribute("open", "false");
-  } else {
-    elm.setAttribute("open", "true");
+async function openEndPt(elm, event) {
+  elmClass = elm.className;
+  try {
+    const resp = await fetch(`/endpoints/${elmClass}.elh`);
+    if (!resp.ok) {
+      throw new Error(`Failed to fetch endpoint ${resp.Status}`);
+    }
+
+    const endPtHTML = await resp.text();
+    const parser = new DOMParser();
+    const endPtDOM = parser.parseFromString(endPtHTML, "text/html");
+    const endPtScript = document.createElement("script");
+    endPtScript.textContent = endPtDOM.querySelector("script").innerText;
+    endPtScript.class = "parsed";
+    endPtDOM.querySelector("script:not(.parsed)").remove();
+    const elms = [
+        endPtDOM.querySelector(".ptName"),
+        endPtDOM.querySelector(".pt"),
+        endPtScript,
+    ];
+    endPtPane.innerHTML = "";
+    for (i = 0; i < elms.length; i++) {
+      endPtPane.appendChild(elms[i]);
+    }
+  } catch (err) {
+    console.error(err);
+    endPtPane.innerHTML = `<h1 style="color: red">`+
+          `err, failed to fetch endpoint: ${err}`;
   }
 }
